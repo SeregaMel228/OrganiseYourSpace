60d52fd9a56ecdc41bb377264ad37115e0d7c32d
user <user@DESKTOP-V4OLSD2>
2017-09-16 11:40:31 +1000

    lo

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(188, 97);
+            this.button1.Location = new System.Drawing.Point(510, 26);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(272, 141);
             this.button1.TabIndex = 0;
